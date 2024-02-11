ef32b1e9caceb354e9e2fc6df1000d2459899bfb
WhiteWolf <95250432+NaumenkoVanya@users.noreply.github.com>
2024-02-11 21:36:29 +0300

    Finish project

---
+++ GuessTheFlag/GuessTheFlag/Work Day/Test 21 day.swift
@@ -0,0 +1,20 @@
+//
+//  Test 21 day.swift
+//  GuessTheFlag
+//
+//  Created by Ваня Науменко on 11.02.24.
+//
+
+import SwiftUI
+
+struct Test_21_day: View {
+    var body: some View {
+        Image("Снимок экрана 2024-02-11 в 21.16.59")
+            .resizable()
+            .ignoresSafeArea()
+    }
+}
+
+#Preview {
+    Test_21_day()
+}
