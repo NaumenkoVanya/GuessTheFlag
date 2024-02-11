57ec54f7e7ba200688868c12075a803fe6a7f3a3
WhiteWolf <95250432+NaumenkoVanya@users.noreply.github.com>
2024-02-11 13:32:34 +0300

    Credit  new project

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Ваня Науменко on 11.02.24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
