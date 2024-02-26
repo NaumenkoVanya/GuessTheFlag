233c1ab3562a87dc15e306236ef4c387a0fe24d2
Naumenko Ivan <95250432+NaumenkoVanya@users.noreply.github.com>
2024-02-26 14:02:27 +0300

    Dev (#1)
    * UpDate
    
    ZStack
    HStack
    VStack
    Colors and frames
    Gradient
    
    * Finish 20 day work
    
    * Finish project
    
    * add extension
    
    add .modifier(FlagImage())
    
    * add  ViewModifier
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,15 +8,111 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        // MARK: Расположение кнопок, Отображение счета игрока с предупреждением,       Стилизация наших флагов, Обновление нашего дизайна
+
+        ZStack {
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)
+            ], center: .top, startRadius: 200, endRadius: 400)
+                .ignoresSafeArea()
+//            LinearGradient(colors: [.blue, .black], startPoint: .top, endPoint: .bottom)
+//                .ignoresSafeArea()
+
+            VStack {
+                Spacer()
+                
+                Text("Guess the flag")
+                    .modifier(Title())
+                    .colorInvert()
+//                    .font(.largeTitle.bold())
+//                    .foregroundStyle(.white)
+                
+                VStack(spacing: 15) {
+                    VStack {
+                        Text("Tap the flag of")
+                            .font(.subheadline.weight(.heavy))
+                            .foregroundStyle(.secondary)
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.weight(.semibold))
+                        // .foregroundStyle(.white)
+                    }
+                    
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            Image(countries[number])
+                                .modifier(FlagImage())
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.regularMaterial)
+                .clipShape(.rect(cornerRadius: 20))
+                
+                Spacer()
+                Spacer()
+                
+                Text("Score: ???")
+                    .modifier(Title())
+                
+                Spacer()
+                
+            }
+            .padding()
+
         }
-        .padding()
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Your score is ???")
+        }
     }
+
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
+        }
+        showingScore = true
+    }
+
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+}
+
+extension ContentView {
+    struct FlagImage: ViewModifier {
+        func body(content: Content) -> some View {
+            content
+                .clipShape(.capsule)
+                .shadow(radius: 5)
+        }
+    }
+    
+    struct Title: ViewModifier {
+            func body(content: Content) -> some View {
+                content
+                    .font(.largeTitle)
+                    .foregroundStyle(.blue)
+                    .padding()
+                    .clipShape(.rect(cornerRadius: 10))
+                    .bold()
+            }
+        }
+
 }
 
 #Preview {
